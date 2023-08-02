cd91915acd4764107fa4f257977219a14b5c6bd6
ppalladio <nine.slide@gmail.com>
2023-08-02 15:15:47 +0200

    built in modules

--- basics/app.js
+++ basics/app.js
@@ -4,14 +4,56 @@
 // console.log(require);
 // console.log(module);
 // console.log(process);
-setInterval(() => {
-	console.log(__dirname);
-}, 1000);
+// setInterval(() => {
+// 	console.log(__dirname);
+// }, 1000);
 
 
 //> module
-const data = require("./module");
-const {name,hi,lastName} = require('./module');
- console.log(data);
-hi(name)
-console.log(lastName);
+// const data = require("./module");
+// const {name,hi,lastName} = require('./module');
+// console.log(data);
+// hi(name)
+// console.log(lastName);
+
+//> built in
+// const os = require('os')
+// console.log(os.userInfo());
+// console.log(os.uptime());
+
+// const path = require('path')
+
+//*separators
+// console.log(path.sep);
+
+// const filePath = path.join('/content', 'hello.txt');
+// const base = path.basename(filePath);
+// //*absolute path
+// const absolutePath = path.resolve(__dirname, 'content', 'hello.txt');
+
+
+// console.log(absolutePath);
+// console.log(base);
+// console.log(filePath);
+
+//> file system
+
+const fs = require('fs')
+fs.writeFileSync('./first.txt', 'first file');
+fs.writeFileSync('./second.txt', 'second file');
+const first = fs.readFileSync('./first.txt','utf8')
+const second = fs.readFileSync('./second.txt','utf8')
+
+//*overwrite/create the file
+fs.writeFileSync('./result.txt',`${first}\n${second}`);
+
+//* concat instead of overwrite
+fs.writeFileSync('./result_concat.txt',`${first}\n${second}`,{flag:'a'});
+
+//> read/writing using callbacks
+fs.readFile('./first.txt', 'utf8', (err, data) => {
+	if(err){
+		throw err;
+	}
+	console.log(data);
+})
