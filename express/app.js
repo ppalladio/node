bba4d5e26bcf2ecec318c25a587c9dee8570216c
ppalladio <nine.slide@gmail.com>
2023-08-06 22:31:24 +0200

    express files set up

--- express/app.js
+++ express/app.js
@@ -1,36 +1,30 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
+//>create a simple page
+// app.get('/', (req, res) => {
+//     res.status(200).send('<h1>Homepage</h1>');
+// });
 
-http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(homePage);
-        res.end();
-    } else if (url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h1>About Page</h1>');
-        res.end();
-    } else if (url === '/styles.css') {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.write(homeStyles);
-        res.end();
-    } else if (url === '/logo.svg') {
-        res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
-        res.write(homeImage);
-        res.end();
-    } else if (url === '/browser-app.js') {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
-        res.write(homeLogic);
-        res.end();
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write('<h1>Page Not Found</h1>');
-        res.end();
-    }
-}).listen(3000);
+// app.get('/about', (req, res) => {
+//     res.status(200).send('<h1>About</h1>');
+// });
+
+// app.all('*', (req, res) => {
+// 	res.status(404).send('<h1>Page not found</h1>');
+
+// });
+
+//> use existing resoures
+//@set up static and middleware
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './navbar-app/index.html'));
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+app.listen(3000, () => {});
