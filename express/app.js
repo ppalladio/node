03a98699deb157394cca1b81269e6c8f41909ac0
ppalladio <nine.slide@gmail.com>
2023-08-06 22:09:03 +0200

    import resources

--- express/app.js
+++ express/app.js
@@ -1,8 +1,11 @@
 const http = require('http');
-const {readFileSync} = require('fs')
+const { readFileSync } = require('fs');
 
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
-const homePage = readFileSync('./index.html','utf8');
 http.createServer((req, res) => {
     const url = req.url;
     if (url === '/') {
@@ -12,6 +15,18 @@ http.createServer((req, res) => {
     } else if (url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<h1>About Page</h1>');
+        res.end();
+    } else if (url === '/styles.css') {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(homeStyles);
+        res.end();
+    } else if (url === '/logo.svg') {
+        res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+        res.write(homeImage);
+        res.end();
+    } else if (url === '/browser-app.js') {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.write(homeLogic);
         res.end();
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
