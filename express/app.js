9f589e622c25654af1439892bbaa045506af11d4
ppalladio <nine.slide@gmail.com>
2023-08-07 23:05:17 +0200

    res.json

--- express/app.js
+++ express/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 //>create a simple page
 // app.get('/', (req, res) => {
@@ -16,15 +15,32 @@ const path = require('path');
 
 // });
 
-//> use existing resoures
+//> use existing resources
 //@set up static and middleware
-app.use(express.static('./public'));
+// const path = require('path');
+// app.use(express.static('./public'));
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './navbar-app/index.html'));
+// });
+
+// app.all('*', (req, res) => {
+//     res.status(404).send('<h1>Page not found</h1>');
+// });
+// app.listen(3000, () => {});
+
+//> SSR /Json
+const { products } = require('./data');
+//@json.stringify
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './navbar-app/index.html'));
+    res.send('<h1>Homepage</h1><a href="/api/products"/>See all products</a>');
+});
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image };
+    });
+    res.json(newProducts);
 });
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Page not found</h1>');
-});
-app.listen(3000, () => {});
+app.listen(5000, () => {});
