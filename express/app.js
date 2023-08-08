0700ed58eda61878dce4272e8e4e6b370bb5f1ee
ppalladio <nine.slide@gmail.com>
2023-08-09 00:42:39 +0200

    query string

--- express/app.js
+++ express/app.js
@@ -32,16 +32,17 @@ const app = express();
 //> SSR /Json
 const { products } = require('./data');
 //@json.stringify
-app.get('/', (req, res) => {
-    res.send('<h1>Homepage</h1><a href="/api/products"/>See all products</a>');
-});
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image };
-    });
-    return res.json(newProducts);
-});
+// app.get('/', (req, res) => {
+//     res.send('<h1>Homepage</h1><a href="/api/products"/>See all products</a>');
+// });
+// app.get('/api/products', (req, res) => {
+//     const newProducts = products.map((product) => {
+//         const { id, name, image } = product;
+//         return { id, name, image };
+//     });
+//     return res.json(newProducts);
+// });
+//> route params
 app.get('/api/products/:productID', (req, res) => {
     const { productID } = req.params;
     const singleProduct = products.find(
@@ -51,6 +52,26 @@ app.get('/api/products/:productID', (req, res) => {
         return res.status(404).send('<h1>Product not found</h1>');
     }
     return res.json(singleProduct);
+});
+//>query string
+
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query);
+    const { search, limit } = req.query; //@search for search and limit key
+    let sortedProducts = [...products];
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.includes(search.toLocaleLowerCase());
+        });
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if ((sortedProducts.length < 1)) {
+        return res.status(200).json({ success: true, data: [] });
+    }
+
+    res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {});
